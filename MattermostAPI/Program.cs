696be67518ac4fcc066c271ee3780fa1afeb6f86
laurieghione <laurie.ghione@hotmail.fr>
2017-01-19 15:34:19 +0100

    add user and channel object

--- MattermostAPI/Program.cs
+++ MattermostAPI/Program.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace MattermostAPI
 {
@@ -12,9 +13,9 @@ namespace MattermostAPI
     {
 
         #region CONST
-        private const string ChannelId = "";
-        private const string TeamId = "";
-        private const string HttpRequest = "https://tcha.com/api/v3";
+        private const string TeamName = "npsix";
+        private const string ChannelName = "test api";
+        private const string HttpRequest = "https://tchat.mailperformance.com/api/v3";
         #endregion  
 
         public static void Main()
@@ -42,27 +43,44 @@ namespace MattermostAPI
             // Get Token by login informations
             PostLogin data = new PostLogin { login_id = loginId, password = password };
             var userLogin = SendPostRequest(client, data, "/users/login");
-            
+
             if (userLogin.Headers.TryGetValues("Token", out values))
             {
+                // Get user by login
+                string getUser = SendGetRequest(client, "/users/me");
+                User user = JsonConvert.DeserializeObject<User>(getUser);
+
+                Console.WriteLine("Bonjour " + user.first_name + " " + user.last_name  + " Authentification réussie");
+
                 string token = values.First();
                 client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
 
-                // Get team
-                string team = SendGetRequest(client, "/teams/all");
+                // Get team by name
+                Console.WriteLine("Récupération des informations de la team");
+
+                string getTeam = SendGetRequest(client, "/teams/name/" + TeamName);
+                Team team = JsonConvert.DeserializeObject<Team>(getTeam);
+
+                Console.WriteLine("Identifiant de la team " + TeamName + "  est : " + team.id);
 
                 // Get channel by name
+                Console.WriteLine("Récupération des informations du channel");
+
+                string getChannel = SendGetRequest(client, "/teams/" + team.id + "/channels/name/" + ChannelName);
+                Channel channel = JsonConvert.DeserializeObject<Channel>(getChannel);
+
+                Console.WriteLine("Identifiant du channel " + ChannelName + "  est : " + channel.id);
 
                 // Post message in a channel   
-                       
                 Console.WriteLine("Veuillez saisir un message a publier : ");
                 string message = Console.ReadLine();
-                PostMessage dataMessage = new PostMessage { message = message, channel_id = ChannelId };
-                string parameters = "/teams/" + TeamId + "/channels/" + ChannelId + "/posts/create";
+                PostMessage dataMessage = new PostMessage { message = message, channel_id = channel.id };
+                string parameters = "/teams/" + team.id + "/channels/" + channel.id + "/posts/create";
 
                 try
                 {
                     SendPostRequest(client, dataMessage, parameters);
+                    Console.WriteLine("Message publié dans le channel");
                 }
                 catch (Exception e)
                 {
@@ -78,7 +96,7 @@ namespace MattermostAPI
         /// <param name="client"> Http client headers</param>
         /// <param name="data"> Payload to post</param>
         /// <param name="parameters">Add parameters in request</param>
-        /// <returns></returns>
+        /// <returns>HttpResponseMessage response</returns>
         public static HttpResponseMessage SendPostRequest(HttpClient client, object data, string parameters)
         {
             string request = HttpRequest + parameters;
@@ -100,7 +118,7 @@ namespace MattermostAPI
         /// </summary>
         /// <param name="client"> Http client headers</param>
         /// <param name="parameters">Add parameters in request</param>
-        /// <returns></returns>
+        /// <returns>string response</returns>
         public static string SendGetRequest(HttpClient client, string parameters)
         {
             string request = HttpRequest + parameters;
@@ -111,7 +129,7 @@ namespace MattermostAPI
             // Response
             response.EnsureSuccessStatusCode();
 
-            return  response.Content.ReadAsStringAsync().Result;
+            return response.Content.ReadAsStringAsync().Result;
         }
     }
 }
