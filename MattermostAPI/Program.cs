9ccde502ed6f3f41fb3dba3af5a1ee59f34f56ce
laurieghione <laurie.ghione@hotmail.fr>
2017-01-19 17:47:40 +0100

    Get channel by name

--- MattermostAPI/Program.cs
+++ MattermostAPI/Program.cs
@@ -14,16 +14,10 @@ namespace MattermostAPI
 
         #region CONST
         private const string TeamName = "npsix";
-        private const string ChannelName = "test api";
         private const string HttpRequest = "https://tchat.mailperformance.com/api/v3";
         #endregion  
 
-        public static void Main()
-        {
-            Start();
-        }
-
-        public static void Start()
+        public static void Main(string[] args)
         {
 
             // Get Authentification informations
@@ -42,51 +36,65 @@ namespace MattermostAPI
 
             // Get Token by login informations
             PostLogin data = new PostLogin { login_id = loginId, password = password };
-            var userLogin = SendPostRequest(client, data, "/users/login");
 
-            if (userLogin.Headers.TryGetValues("Token", out values))
+            try
             {
-                // Get user by login
-                string getUser = SendGetRequest(client, "/users/me");
-                User user = JsonConvert.DeserializeObject<User>(getUser);
+                var userLogin = SendPostRequest(client, data, "/users/login");
 
-                Console.WriteLine("Bonjour " + user.first_name + " " + user.last_name  + " Authentification réussie");
-
-                string token = values.First();
-                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
-
-                // Get team by name
-                Console.WriteLine("Récupération des informations de la team");
-
-                string getTeam = SendGetRequest(client, "/teams/name/" + TeamName);
-                Team team = JsonConvert.DeserializeObject<Team>(getTeam);
-
-                Console.WriteLine("Identifiant de la team " + TeamName + "  est : " + team.id);
-
-                // Get channel by name
-                Console.WriteLine("Récupération des informations du channel");
-
-                string getChannel = SendGetRequest(client, "/teams/" + team.id + "/channels/name/" + ChannelName);
-                Channel channel = JsonConvert.DeserializeObject<Channel>(getChannel);
-
-                Console.WriteLine("Identifiant du channel " + ChannelName + "  est : " + channel.id);
-
-                // Post message in a channel   
-                Console.WriteLine("Veuillez saisir un message a publier : ");
-                string message = Console.ReadLine();
-                PostMessage dataMessage = new PostMessage { message = message, channel_id = channel.id };
-                string parameters = "/teams/" + team.id + "/channels/" + channel.id + "/posts/create";
-
-                try
+                if (userLogin.Headers.TryGetValues("Token", out values))
                 {
-                    SendPostRequest(client, dataMessage, parameters);
-                    Console.WriteLine("Message publié dans le channel");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Erreur : " + e);
-                }
+                    // Put header authentification
+                    string token = values.First();
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+                    Console.WriteLine("Authentification réussie");
 
+                    // Get user by login
+                    string getUser = SendGetRequest(client, "/users/me");
+                    User user = JsonConvert.DeserializeObject<User>(getUser);
+
+                    Console.WriteLine(string.Format("Bonjour {0} {1}", user.first_name, user.last_name));
+
+                    // Get team by name
+                    Console.WriteLine("Récupération des informations de la team");
+
+                    string getTeam = SendGetRequest(client, string.Format("/teams/name/{0}", TeamName));
+                    Team team = JsonConvert.DeserializeObject<Team>(getTeam);
+
+                    Console.WriteLine(string.Format("Identifiant de la team {0} est : {1}", TeamName, team.id));
+
+                    // Get channel by name
+                    Console.WriteLine("Veuillez saisir le nom du channel");
+                    string channelName = Console.ReadLine();
+                    Console.WriteLine("Récupération des informations du channel");
+
+                    string getChannel = SendGetRequest(client, string.Format("{0}/teams/{1}/channels/name/{2}", client, team.id, channelName));
+                    Channel channel = JsonConvert.DeserializeObject<Channel>(getChannel);
+
+                    Console.WriteLine(string.Format("Identifiant du channel {0} est : {1}", channelName, channel.id));
+
+                    // Post message in a channel   
+                    Console.WriteLine("Veuillez saisir un message a publier : ");
+                    string message = Console.ReadLine();
+                    PostMessage dataMessage = new PostMessage { message = message, channel_id = channel.id };
+                    string parameters = string.Format("/teams/{0}/channels/{1}/posts/create", team.id, channel.id);
+
+                    try
+                    {
+                        SendPostRequest(client, dataMessage, parameters);
+                        Console.WriteLine("Message publié dans le channel");
+                        Console.ReadKey();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Erreur " + e);
+                        Console.ReadKey();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur d'authentification" + e);
+                Console.ReadKey();
             }
         }
 
